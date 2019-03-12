d9291b7672b67ef82324748d6d7f3e1c17087f36
nkostoulas <nkostoulas@gmail.com>
2019-03-12 15:28:57 +0000

    add correct challenge sig message

--- examples/test_client.rs
+++ examples/test_client.rs
@@ -12,9 +12,15 @@ use hyper::{Body, Method, Request};
 fn main() {
     let client = Client::new();
 
-    let json = r#"{"hash":"0606060606060606060606060606060606060606060606060606060606060606"}"#;
+    let data = r#"
+    {
+        "hash": "0404040404040404040404040404040404040404040404040404040404040404",
+        "pubkey": "0325bf82856a8fdcc7a2c08a933343d2c6332c4c252974d6b09b6232ea40804626",
+        "sig": "304402201742daea5ec3b7306b9164be862fc1659cc830032180b8b17beffe02645860d602201039eba402d22e630308e6af05da8dd4f05b51b7d672ca5fc9e3b0a57776365c"
+    }"#;
+
     let uri: hyper::Uri = "http://localhost:9999/challengeproof".parse().unwrap();
-    let mut req = Request::new(Body::from(json));
+    let mut req = Request::new(Body::from(data));
     *req.method_mut() = Method::POST;
     *req.uri_mut() = uri.clone();
     req.headers_mut().insert(
