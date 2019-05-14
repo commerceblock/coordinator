f77b0e73b8876e1543ce637a6bd99e8cedca449e
nkostoulas <nkostoulas@gmail.com>
2019-05-14 09:55:15 +0100

    Move authorization out of api server main

--- src/api.rs
+++ src/api.rs
@@ -8,7 +8,7 @@ use std::thread;
 
 use base64::decode;
 use bitcoin_hashes::sha256d;
-use hyper::StatusCode;
+use hyper::{Body, Request, StatusCode};
 use jsonrpc_http_server::jsonrpc_core::{Error, IoHandler, Params, Value};
 use jsonrpc_http_server::{hyper::header, AccessControlAllowOrigin, DomainsValidation, Response, ServerBuilder};
 use serde::{Deserialize, Serialize};
@@ -27,6 +27,8 @@ struct GetChallengeResponsesResponse {
     responses: Vec<ChallengeResponseIds>,
 }
 
+/// Get challenge responses RPC call returning all responses for a specific
+/// request transaction id hash
 fn get_challenge_responses(params: Params, storage: Arc<Storage>) -> Result<Value, Error> {
     let try_parse = params.parse::<GetChallengeResponsesParams>();
     match try_parse {
@@ -39,8 +41,27 @@ fn get_challenge_responses(params: Params, storage: Arc<Storage>) -> Result<Valu
     }
 }
 
-/// Run Api server for external requests that require information from the
-/// coordinator
+/// Do basic authorization on incoming request by parsing the AUTHORIZATION
+/// header decoding username/password and comparing with config
+fn authorize(our_auth: &str, request: &Request<Body>) -> bool {
+    let auth = request
+        .headers()
+        .get(header::AUTHORIZATION)
+        .map(|h| h.to_str().unwrap_or("").to_owned());
+    if let Some(auth_basic) = auth {
+        let auth_parts: Vec<&str> = auth_basic.split(" ").collect();
+        if auth_parts.len() == 2 {
+            let auth_basic = &decode(auth_parts[1]).unwrap();
+            let auth_basic_str = str::from_utf8(&auth_basic).unwrap();
+            return auth_basic_str == our_auth;
+        }
+    }
+    false
+}
+
+/// Run Api RPC server for external requests that require information from the
+/// coordinator. Data returned to the caller are drawn from the storage
+/// interface which is shared with the main coordinator process
 pub fn run_api_server<D: Storage + Send + Sync + 'static>(
     config: &ApiConfig,
     storage: Arc<D>,
@@ -53,29 +74,14 @@ pub fn run_api_server<D: Storage + Send + Sync + 'static>(
     let our_auth = format! {"{}:{}", config.user, config.pass};
     let server = ServerBuilder::new(io)
         .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
-        .request_middleware(move |request: hyper::Request<hyper::Body>| {
-            if our_auth != "" {
-                let mut passed_auth = false;
-                let auth = request
-                    .headers()
-                    .get(header::AUTHORIZATION)
-                    .map(|h| h.to_str().unwrap_or("").to_owned());
-                if let Some(auth_basic) = auth {
-                    let auth_parts: Vec<&str> = auth_basic.split(" ").collect();
-                    if auth_parts.len() == 2 {
-                        let auth_basic = &decode(auth_parts[1]).unwrap();
-                        let auth_basic_str = str::from_utf8(&auth_basic).unwrap();
-                        passed_auth = auth_basic_str == our_auth;
-                    }
+        .request_middleware(move |request: Request<Body>| {
+            if our_auth != "" && !authorize(&our_auth, &request) {
+                return Response {
+                    code: StatusCode::UNAUTHORIZED,
+                    content_type: header::HeaderValue::from_str("text/plain").unwrap(),
+                    content: "Bad Authorization Attempt".to_string(),
                 }
-                if !passed_auth {
-                    return Response {
-                        code: StatusCode::UNAUTHORIZED,
-                        content_type: header::HeaderValue::from_str("text/plain").unwrap(),
-                        content: "Bad Authorization Attempt".to_string(),
-                    }
-                    .into();
-                }
+                .into();
             }
             request.into()
         })
