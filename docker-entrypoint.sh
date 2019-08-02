6a12b766d0f75fefa0df0e177d7441365f036f8c
Paulius Stakauskas <paulius@zenlot.xyz>
2019-08-02 09:29:54 +0100

    Add client and service pass secrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,14 @@ if [ -f /run/secrets/coordinator_pass ]; then
     export CO_API_PASS="$(cat /run/secrets/coordinator_pass)"
 fi
 
+if [ -f /run/secrets/ocean_pass ]; then
+    export CO_CLIENTCHAIN_PASS="$(cat /run/secrets/ocean_pass)"
+fi
+
+if [ -f /run/secrets/service_pass ]; then
+    export CO_SERVICE_PASS="$(cat /run/secrets/oceanservice_pass)"
+fi
+
 case "$1" in
         coordinator)
             echo "Running coordinator"
