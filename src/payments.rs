715aaa87b1bcea784e0107114c90985d699e49f3
nkostoulas <nkostoulas@gmail.com>
2019-11-19 17:09:48 +0000

    Create Payments struct

--- src/payments.rs
+++ src/payments.rs
@@ -60,94 +60,117 @@ fn calculate_bid_payment(fee_amount: &Amount, fee_percentage: u64, num_bids: u64
     Ok(gn_amount_per_gn)
 }
 
-/// TODO
-fn pay_bids(_client: &OceanClient, _clientchain_config: &ClientChainConfig, _bids: &BidSet) -> Result<()> {
-    // pay
-    // set bid txid
-    // update bid
-    Ok(())
-}
-
-/// TODO
-fn process_bids(
-    bids: &BidSet,
-    bids_amount: &Amount,
-    response: &Response,
-    addr_params: &'static AddressParams,
-) -> Result<()> {
-    for bid in bids {
-        if let Some(bid_resp) = response.bid_responses.get(&bid.txid) {
-            let gn_amount_corrected = *bids_amount * (*bid_resp).into() / response.num_challenges.into();
-            let das_pub = PublicKey {
-                key: bid.pubkey,
-                compressed: true,
-            };
-            let gn_pay_to_addr = Address::p2pkh(&das_pub, None, addr_params);
-            info!(
-                "bid: {}\naddr: {}\ngn_amount_corrected: {}\n",
-                bid.txid, gn_pay_to_addr, gn_amount_corrected
-            );
-
-            // set bid amount, addr
-            // update bid
-        }
-    }
-    Ok(())
-}
-
 /// TODO: add comments
-fn do_request_payment(
-    clientchain_config: &ClientChainConfig,
-    request: &Request,
-    client: &OceanClient,
-    storage: &Arc<dyn Storage>,
-) -> Result<()> {
-    let bids = storage.get_bids(request.txid)?;
-    if bids.len() > 0 {
-        if let Some(resp) = storage.get_response(request.txid)? {
-            let amount = calculate_fees(request, client)?;
-            let bid_payment = calculate_bid_payment(&amount, request.fee_percentage.into(), bids.len() as u64)?;
-            process_bids(
-                &bids,
-                &bid_payment,
-                &resp,
-                get_chain_addr_params(&clientchain_config.chain),
-            )?;
-            //pay_bids()?;
-        }
-    }
-
-    // set request complete
-    // update request
-
-    Ok(())
-}
-
-/// TODO: add comments
-fn do_request_payments(
-    clientchain_config: ClientChainConfig,
+pub struct Payments {
+    storage: Arc<dyn Storage + Send + Sync>,
+    config: ClientChainConfig,
     client: OceanClient,
-    storage: Arc<dyn Storage>,
-    req_recv: Receiver<sha256d::Hash>,
-) -> Result<()> {
-    // First pay out any past requests that have not been fully paid yet
-    // TODO: only get incomplete only from storage
-    let incomplete_requests = storage.get_requests()?;
-    for req in incomplete_requests {
-        let _ = do_request_payment(&clientchain_config, &req, &client, &storage)?;
+    addr_params: &'static AddressParams,
+}
+
+impl Payments {
+    /// TODO
+    fn pay_bids(&self, _bids: &BidSet) -> Result<()> {
+        // pay
+        // set bid txid
+        // update bid
+        Ok(())
     }
 
-    // Wait for new requests
-    loop {
-        match req_recv.recv() {
-            Ok(resp) => {
-                let req = storage.get_request(resp)?.unwrap();
-                let _ = do_request_payment(&clientchain_config, &req, &client, &storage)?;
-            }
-            Err(RecvError) => {
-                return Err(Error::from(CError::ReceiverDisconnected));
+    /// TODO
+    fn process_bids(&self, bids: &BidSet, bid_payment: &Amount, response: &Response) -> Result<()> {
+        for bid in bids {
+            if let Some(bid_resp) = response.bid_responses.get(&bid.txid) {
+                let gn_amount_corrected = *bid_payment * (*bid_resp).into() / response.num_challenges.into();
+                let das_pub = PublicKey {
+                    key: bid.pubkey,
+                    compressed: true,
+                };
+                let gn_pay_to_addr = Address::p2pkh(&das_pub, None, self.addr_params);
+                info!(
+                    "bid: {}\naddr: {}\ngn_amount_corrected: {}\n",
+                    bid.txid, gn_pay_to_addr, gn_amount_corrected
+                );
+
+                // set bid amount, addr
+                // update bid
             }
         }
+        Ok(())
+    }
+
+    /// TODO: add comments
+    fn do_request_payment(&self, request: &Request) -> Result<()> {
+        let bids = self.storage.get_bids(request.txid)?;
+        if bids.len() > 0 {
+            if let Some(resp) = self.storage.get_response(request.txid)? {
+                let amount = calculate_fees(request, &self.client)?;
+                let bid_payment = calculate_bid_payment(&amount, request.fee_percentage.into(), bids.len() as u64)?;
+                self.process_bids(&bids, &bid_payment, &resp)?;
+                self.pay_bids(&bids)?;
+            }
+        }
+
+        // set request complete
+        // update request
+
+        Ok(())
+    }
+
+    /// TODO: add comments
+    fn do_request_payments(&self, req_recv: Receiver<sha256d::Hash>) -> Result<()> {
+        // First pay out any past requests that have not been fully paid yet
+        // TODO: only get incomplete only from storage
+        let incomplete_requests = self.storage.get_requests()?;
+        for req in incomplete_requests {
+            let _ = self.do_request_payment(&req)?;
+        }
+
+        // Wait for new requests
+        loop {
+            match req_recv.recv() {
+                Ok(resp) => {
+                    let req = self.storage.get_request(resp)?.unwrap();
+                    let _ = self.do_request_payment(&req)?;
+                }
+                Err(RecvError) => {
+                    return Err(Error::from(CError::ReceiverDisconnected));
+                }
+            }
+        }
+    }
+
+    /// TODO: add comments
+    pub fn new(clientchain_config: ClientChainConfig, storage: Arc<dyn Storage + Send + Sync>) -> Result<Payments> {
+        let client = OceanClient::new(
+            clientchain_config.host.clone(),
+            Some(clientchain_config.user.clone()),
+            Some(clientchain_config.pass.clone()),
+        )?;
+
+        let addr_params = get_chain_addr_params(&clientchain_config.chain);
+        if let Some(addr) = &clientchain_config.payment_addr {
+            let ocean_addr = Address::from_str(&addr)?;
+            if *ocean_addr.params != *addr_params {
+                warn!("payment addr and chain config addr param mismatch");
+            } else if let Some(key) = &clientchain_config.payment_key {
+                let addr_unspent = client.list_unspent(None, None, Some(&[ocean_addr]), None, None)?;
+                if addr_unspent.len() == 0 {
+                    client.import_priv_key(key, None, Some(true))?;
+                }
+            } else {
+                warn!("payment key missing");
+            }
+        } else {
+            warn!("payment addr missing");
+        }
+
+        Ok(Payments {
+            storage: storage,
+            config: clientchain_config,
+            client: client,
+            addr_params: addr_params,
+        })
     }
 }
 
@@ -157,31 +180,10 @@ pub fn run_payments(
     storage: Arc<dyn Storage + Send + Sync>,
     req_recv: Receiver<sha256d::Hash>,
 ) -> Result<thread::JoinHandle<()>> {
-    let client = OceanClient::new(
-        clientchain_config.host.clone(),
-        Some(clientchain_config.user.clone()),
-        Some(clientchain_config.pass.clone()),
-    )?;
-
-    if let Some(addr) = &clientchain_config.payment_addr {
-        let ocean_addr = Address::from_str(&addr)?;
-        if *ocean_addr.params != *get_chain_addr_params(&clientchain_config.chain) {
-            warn!("payment addr and chain config addr param mismatch");
-        } else if let Some(key) = &clientchain_config.payment_key {
-            let addr_unspent = client.list_unspent(None, None, Some(&[ocean_addr]), None, None)?;
-            if addr_unspent.len() == 0 {
-                client.import_priv_key(key, None, Some(true))?;
-            }
-        } else {
-            warn!("payment key missing");
-        }
-    } else {
-        warn!("payment addr missing");
-    }
-
+    let payments = Payments::new(clientchain_config, storage)?;
     Ok(thread::spawn(move || {
-        if let Err(err) = do_request_payments(clientchain_config, client, storage.clone(), req_recv) {
-            error! {"payments error: {}", err}
+        if let Err(err) = payments.do_request_payments(req_recv) {
+            error! {"payments error: {}", err};
         }
     }))
 }
